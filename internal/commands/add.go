53240d5c45ff4d57fc46b752fa500cda86a1f229
Niels Soete <soeteniels1803@gmail.com>
2025-03-07 14:54:14 +0100

    feat(files/directories are now being moved to $HOME/.dotfiles and symlinks are created to their orignal location)

--- internal/commands/add.go
+++ internal/commands/add.go
@@ -3,23 +3,41 @@ package commands
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func Add(path string) {
 
-	if !isInitialized() {
+	repoPath := os.ExpandEnv("$HOME/.dotfiles")
+
+	if !isInitialized(repoPath) || !isPathExist(path) {
 		return
 	}
 
-	if !isPathExist(path) {
+	absFilePath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error getting absolute path:", err)
 		return
 	}
 
-	fmt.Println(path)
+	fileName := filepath.Base(absFilePath)
+	targetPath := filepath.Join(repoPath, fileName)
+
+	err = os.Rename(absFilePath, targetPath)
+	if err != nil {
+		fmt.Println("Error moving file/directory:", err)
+		return
+	}
+
+	err = os.Symlink(targetPath, absFilePath)
+	if err != nil {
+		fmt.Println("Error creating symlink: ", err)
+	}
+
+	fmt.Println("Successfully added", fileName, "to dotman")
 }
 
-func isInitialized() bool {
-	repoPath := os.ExpandEnv("$HOME/.dotfiles")
+func isInitialized(repoPath string) bool {
 
 	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
 		fmt.Println("Error: Dotfile repository does not exist at", repoPath)

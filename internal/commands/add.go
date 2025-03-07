d3421387d9f366f4e2c6bad0cc92d5ece896196a
Niels Soete <soeteniels1803@gmail.com>
2025-03-07 10:20:12 +0100

    fix(refactor)

--- internal/commands/add.go
+++ internal/commands/add.go
@@ -7,19 +7,35 @@ import (
 
 func Add(path string) {
 
+	if !isInitialized() {
+		return
+	}
+
+	if !isPathExist(path) {
+		return
+	}
+
+	fmt.Println(path)
+}
+
+func isInitialized() bool {
 	repoPath := os.ExpandEnv("$HOME/.dotfiles")
 
 	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
 		fmt.Println("Error: Dotfile repository does not exist at", repoPath)
 		fmt.Println("Please run 'dotman init' to initialize the repository.")
 		fmt.Println("For more information, run 'dotman help'.")
-		return
+		return false
 	}
 
+	return true
+}
+
+func isPathExist(path string) bool {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		fmt.Println("Path not found:", path)
-		return
+		return false
 	}
 
-	fmt.Println(path)
+	return true
 }
